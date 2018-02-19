4f854dee557b6e25e91b02789c94f22d7b4c73b0
Benny Johansson <benny.json@gmail.com>
2018-02-19 20:58:26 +0000

    Add rest API to access items and stores

---
+++ security.py
@@ -0,0 +1,28 @@
+from werkzeug.security import safe_str_cmp
+from models.user import UserModel
+
+users = [
+    UserModel('Bob', 'asdf')
+]
+
+username_mapping = {u.username: u for u in users}
+
+user_id_mapping = {
+    1: {
+        'id': 1,
+        'username': 'bob',
+        'password': 'asdf'
+    }
+}
+
+
+def authenticate(username, password):
+    user = UserModel.find_by_username(username)
+
+    if user and safe_str_cmp(user.password, password):
+        return user
+
+
+def identity(payload):
+    user_id = payload['identity']
+    return UserModel.find_by_id(user_id)
